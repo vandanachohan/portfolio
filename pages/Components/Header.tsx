62064edb034fe4870a910987a7dcd2cc595f5b8e
vandanachohan <161231376+vandanachohan@users.noreply.github.com>
2024-11-10 18:59:33 +0500

    header changes

--- pages/Components/Header.tsx
+++ pages/Components/Header.tsx
@@ -1,20 +1,39 @@
-import React from 'react'
+// import React from 'react'
+
+// const Header = () => {
+//   return (
+//     <header className="header">
+//     <span><h3><b>Vandana Chohan</b></h3></span>
+//     <nav className='navbar'>
+//         <a href="/">Home</a>
+//         <a href="/about">About</a>
+//         <a href="/skills">Skills</a>
+//         <a href="/project">Projects</a>
+//         <a href="/contact">Contact</a>
+//     </nav>
+//     </header>
+//   )
+// }
+
+// export default Header
+
+
+import React from 'react';
+import Link from 'next/link';
 
 const Header = () => {
   return (
     <header className="header">
-    <span><h3><b>Vandana Chohan</b></h3></span>
-    <nav className='navbar'>
-        <a href="/">Home</a>
-        <a href="/about">About</a>
-        <a href="/skills">Skills</a>
-        <a href="/project">Projects</a>
-        <a href="/contact">Contact</a>
-    </nav>
+      <span><h3><b>Vandana Chohan</b></h3></span>
+      <nav className="navbar">
+        <Link href="/">Home</Link>
+        <Link href="/about">About</Link>
+        <Link href="/skills">Skills</Link>
+        <Link href="/project">Projects</Link>
+        <Link href="/contact">Contact</Link>
+      </nav>
     </header>
-  )
+  );
 }
 
-export default Header
-
-
+export default Header;
