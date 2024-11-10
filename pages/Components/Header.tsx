f753e2d05533da441ea0d839b3e6c41f0389abad
vandanachohan <161231376+vandanachohan@users.noreply.github.com>
2024-11-10 18:33:21 +0500

    link tag change

--- pages/Components/Header.tsx
+++ pages/Components/Header.tsx
@@ -1,18 +1,60 @@
-import React from 'react'
+// import React from 'react'
 
-const Header = () => {
+// const Header = () => {
+//   return (
+//     <header className="header">
+//     <span><h3><b>Vandana Chohan</b></h3></span>
+//     <nav className='navbar'>
+//         <a href="/">Home</a>
+//         <a href="/about">About</a>
+//         <a href="/skills">Skills</a>
+//         <a href="/project">Projects</a>
+//         <a href="/contact">Contact</a>
+//     </nav>
+//     </header>
+//   )
+// }
+
+// export default Header
+
+// Header.tsx
+import React from 'react';
+import Link from 'next/link';
+
+const Header: React.FC = () => {
   return (
-    <header className="header">
-    <span><h3><b>Vandana Chohan</b></h3></span>
-    <nav className='navbar'>
-        <a href="/">Home</a>
-        <a href="/about">About</a>
-        <a href="/skills">Skills</a>
-        <a href="/project">Projects</a>
-        <a href="/contact">Contact</a>
-    </nav>
+    <header>
+      <nav>
+        <ul>
+          <li>
+            <Link href="/" passHref>
+              <a>Home</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/about" passHref>
+              <a>About</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/skills" passHref>
+              <a>Skills</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/project" passHref>
+              <a>Project</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/contact" passHref>
+              <a>Contact</a>
+            </Link>
+          </li>
+        </ul>
+      </nav>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
